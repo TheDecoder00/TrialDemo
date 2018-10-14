8141073f0b071a6257a4f6c3948170d4cd49e4af
Utkarsh Singh <utk01r@gmail.com>
2018-10-14 17:31:52 +0530

    adding extra here

--- dropdown.js
+++ dropdown.js
@@ -10,7 +10,17 @@ describe("Testing Dropdwon at https://demos.telerik.com/kendo-ui/dropdownlist/an
 		element.all(by.css(".demo-section k-content > select  option")).then(function(items){
 			console.log("Total values in Dropdown is : "+items.length);
 		});		
+		//addighd fdj skodn skfmdf  sjlfd
 		
-		
+		it("Selecting dropdwon3", function(){
+			browser.driver;
+			browser.driver.manage().window().maximize();
+			browser.get("https://demos.telerik.com/kendo-ui/dropdownlist/angular");
+			
+			browser.sleep(6000);
+			
+			element.all(by.css(".demo-section k-content > select  option")).then(function(items){
+				console.log("Total values in Dropdown is : "+items.length);
+			});		
 	});
 });
